d746de8fa77e0a18af8d4c74fe2e13934f9f34ba
Tian Ge <greenteak1992@gmail.com>
2018-07-29 23:09:11 +0800

    finished

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,5 +1,6 @@
-require('items.js')
-require('promotions.js')
+'use strict'
+const loadAllItems = require('../src/items.js')
+const loadPromotions = require('../src/promotions.js')
 function bestCharge(selectedItems) {
   let orderForm = `============= 订餐明细 =============\n`;
   const allItem = loadAllItems();
@@ -8,13 +9,17 @@ function bestCharge(selectedItems) {
   let firstDiscount = 6;
   let secondDiscount = 0;
   let secondDiscountName = [];
+
+  //将输入中每个item的barcode和value提出来
   const itemAndNum = selectedItems.map((item) => {
     let itemValue = item.split(' x ');
     return {key: itemValue[0], value: itemValue[1]}
   })
+
+  //计算每项的价位
   for (let i = 0; i < itemAndNum.length; i++) {
     let priceNoCount = allItem.filter((item) => item.id === itemAndNum[i].key)
-    let itemCount = priceNoCount[0].name * itemAndNum[i].value;
+    let itemCount = priceNoCount[0].price * itemAndNum[i].value;
     let itemStr = `${priceNoCount[0].name} x ${itemAndNum[i].value} = ${itemCount}元\n`
     orderForm += itemStr;
     totalCount += itemCount;
@@ -23,20 +28,30 @@ function bestCharge(selectedItems) {
       secondDiscountName.push(priceNoCount[0].name);
     }
   }
+
+  //不使用优惠
   orderForm += `-----------------------------------\n`;
+  if(secondDiscount == 0 &&totalCount < 30){
+    orderForm += `总计：${totalCount}元\n===================================`
+    return orderForm;
+  }
+
+  //使用哪种优惠
   let haveDiscount = `使用优惠:\n`;
   if(secondDiscount > firstDiscount){
-     haveDiscount += promotions[1].type + "，"+secondDiscountName.join("，")+`\n`;
+     //haveDiscount += promotions[1].type + "("+secondDiscountName.join("，")+`)，\n`;
+     haveDiscount +=`${promotions[1].type}(${secondDiscountName.join("，")})，省${secondDiscount}元\n`
      totalCount -= secondDiscount;
   }
-  if(secondDiscountName <= firstDiscount && totalCount >= 30){
+  if(secondDiscount <= firstDiscount && totalCount >= 30){
     haveDiscount += promotions[0].type + `，省6元\n`;
     totalCount -= firstDiscount;
   }
-  orderForm += haveDiscount;
-  if(secondDiscount == 0 &&totalCount < 30){
-    orderForm += `总计：${totalCount}元\n===================================`
-  }
+
+  //计算总的orderForm
+  orderForm += haveDiscount + `-----------------------------------\n`;
+  orderForm += `总计：${totalCount}元\n===================================`
   return orderForm;
 }
+module.exports = bestCharge
 
